47075d5ceabe503b0f015ffeb819043908c52395
cawatou <cawatou@gmail.com>
2017-10-17 17:52:48 +0300

    order_list css

--- src/html/js/main.js
+++
@@ -1,9 +0,0 @@
-$(".yes .more").click(function () {
-    $(this).toggleClass("active");
-    $(".hidden").slideToggle("300");
-});
-(function ($) {
-    $(function () {
-        $('input, select').styler();
-    });
-})(jQuery);
