786ca241c01fd4da8c73754332cb51a98080c2f1
ljkh <cawatou@gmail.com>
2017-10-27 20:14:05 +0300

    all order on map

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -12,10 +12,12 @@ export class MapPage {
     items: any;
     markers: any[];
 
+    markerObjects : any;
 
     constructor(public navCtrl:NavController, public navParams:NavParams, public api:Api) {
         this.items = navParams.get('items');
 
+        console.log(this.items);
         var coord = [];
         for (var i = 0; i < this.items.length; i++) {
             this.api.getMapCoord(this.items[i].Address)
@@ -28,9 +30,37 @@ export class MapPage {
         }
 
         this.markers = coord;
+
+        let fs = new Array<any>();
+        for(var i= 0; i< 5000; i++){
+            fs.push(this.getFeature(i));
+        }
+
+        this.markerObjects = {
+            "type": "FeatureCollection",
+            "features": fs
+        };
+
+
     }
 
     ionViewDidLoad() {
     }
 
+
+    getFeature(id: number){
+        return   {"type": "Feature", "id": id, "geometry": {"type": "Point", "coordinates": [this.random(55, 57, id), this.random(36, 38, id)]}, "properties": {"balloonContent": "Содержимое балуна", "clusterCaption": "Еще одна метка", "hintContent": "Текст подсказки"}};
+    }
+
+    random(min: number, max: number, id: number){
+
+        let m = id % 333;
+        var random = Math.floor(Math.random() *(max-min+m)+min);
+        if(random < 0) {
+            return random * -1;
+        }
+
+        return random;
+
+    }
 }
