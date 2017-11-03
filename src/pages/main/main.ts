edcb3703b214b5b5c6e90f7b220a42156f587e1c
cawatou <cawatou@gmail.com>
2017-11-03 19:45:12 +0300

    hot fix

--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -1,17 +1,25 @@
-import {Component}                              from '@angular/core';
-import {IonicPage, NavController, NavParams}    from 'ionic-angular';
+import { Component }                   from '@angular/core';
+import { IonicPage, NavController }    from 'ionic-angular';
+import { Api }                         from "../../providers/api/api";
+import { Storage }                     from '@ionic/storage';
 
- 
 @IonicPage()
 @Component({
     selector: 'page-main',
     templateUrl: 'main.html'
 })
 export class MainPage {
-    item:any;
+    params: any;
+    user: any;
 
-    constructor(public navCtrl:NavController, navParams:NavParams) {
-
+    constructor(public navCtrl: NavController, public api: Api, private storage: Storage) {
+        this.storage.get('user').then(val => {
+            this.user = val;
+            console.log(val);
+            // this.params = ['requests', '0', this.user.Master, '1', '3'];
+            // this.api.get(this.params)
+            //     .subscribe(data => this.items = data.json());
+        });
     }
 
     openContract() {
--- src/pages/order-list/order-list.ts
+++ src/pages/order-list/order-list.ts
@@ -27,7 +27,7 @@ export class OrderListPage {
 
     constructor(public navCtrl: NavController, public api: Api, private storage: Storage) {
         this.storage.get('user').then(val => {
-            this.user = val
+            this.user = val;
             this.params = ['requests', '0', this.user.Master, '1', '3'];
             this.api.get(this.params)
                 .subscribe(data => this.items = data.json());
