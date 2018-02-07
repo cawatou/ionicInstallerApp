267b96095c482cf5f4271002897c87ac0ec6d1a9
cawatou <cawatou@gmail.com>
2018-02-07 13:53:22 +0300

    map (p2 - show all markers)

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,93 +1,69 @@
-import { Component }                              from '@angular/core';
-import { IonicPage, NavController, NavParams }    from 'ionic-angular';
+import { Component, ViewChild, ElementRef }       from '@angular/core';
+import { IonicPage, NavController, Platform }     from 'ionic-angular';
 import { Api }                                    from "../../providers/api/api";
 import { Storage }                                from '@ionic/storage';
+
+declare var google: any;
 
 @IonicPage()
 @Component({
     selector: 'page-map',
     templateUrl: 'map.html'
 })
+
 export class MapPage {
     items: any;
     markers: any[];
     params: any;
     user: any;
-    markerObjects : any;
 
-    constructor(
-        public navCtrl:NavController,
-        public api:Api,
-        private storage: Storage ) {
+    @ViewChild('mapCanvas') mapElement: ElementRef;
+    constructor(public navCtrl:NavController, public api:Api, private storage: Storage ) {}
 
-        var tt;
-
+    ionViewDidLoad() {
         this.storage.get('user').then(val => {
             this.user = val;
             this.params = ['requests', '0', this.user.Master, '1', '3'];
             this.api.get(this.params).subscribe(data => {
                 this.items = data.json();
-                console.log(this.items);
-                let features = [];
+                //console.log(this.items);
+
+                let mapEle = this.mapElement.nativeElement;
+
+                let map = new google.maps.Map(mapEle, {
+                    center: {lat: 59.9358298, lng: 30.2857049},
+                    zoom: 11
+                });
+
                 for (var i = 0; i < this.items.length; i++) {
-                    this.api.getMapCoord(this.items[i].Address)
-                        .subscribe(data => {
-                            console.log(data);
+                    this.api.getMapCoord(this.items[i].Address).subscribe(data => {
+                        let coord = data.json().results[0].geometry.location;
+                        coord.name = data.json().results[0].formatted_address;
 
-                            // let res = data.json().response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos.split(' ');
-                            // let lon = res[0];
-                            // let lat = res[1];
-                            // features.push(this.getFeature(lat, lon));
+                        let infoWindow = new google.maps.InfoWindow({
+                            content: `<h5>${coord.name}</h5>`
                         });
+
+                        let marker = new google.maps.Marker({
+                            position: coord,
+                            map: map,
+                            title: coord.name
+                        });
+
+                        marker.addListener('click', () => {
+                            infoWindow.open(map, marker);
+                        });
+
+                        google.maps.event.addListenerOnce(map, 'idle', () => {
+                            mapEle.classList.add('show-map');
+                        });
+                    });
                 }
-
-                // console.log(features);
-                //
-                // this.markerObjects = {
-                //     "type": "FeatureCollection",
-                //     "features": features
-                // };
-
             })
         })
-
-       /*
-*/
-
     }
 
-    getFeature(lat: string, lon: string){
-        return   {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [lat, lon]
-            },
-            "properties": {
-                "balloonContent": "Содержимое балуна",
-                "clusterCaption": "Еще одна метка",
-                "hintContent": "Текст подсказки"
-            }
-        };
-    }
-
-    openScheduler(item) {
-        this.navCtrl.push('SchedulerPage', {
-            item: item
-        });
-    }
-
-    openMap(items) {
-        this.navCtrl.push('MapPage', {
-            items: items
-        });
-    }
-
-    openMain() {
-        this.navCtrl.push('MainPage');
-    }
-
-    openProfile() {
-        this.navCtrl.push('ProfilePage');
+    openPage(page){
+        this.navCtrl.setRoot(page);
     }
 }
